df2b544f1ef569ef327bc00a1ea0e1f9c4c845ca
JoRan29 <jokavana29@gmail.com>
2020-04-10 19:46:26 +0200

    function

--- codecamp/src/index.js
+++ codecamp/src/index.js
@@ -1,4 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+function MyApp() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>This is a paragraph!</p>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(<MyApp />, document.getElementById("root"));
